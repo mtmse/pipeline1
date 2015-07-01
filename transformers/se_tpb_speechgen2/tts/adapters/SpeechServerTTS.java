91b2dcf4fbc56d4b83a56516015173f82adefa32
Thomas Sundberg <tsu@kth.se>
2015-07-01 14:21:50 +0200

    Initiated the implementation of a speech server tts

---
+++ transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTS.java
@@ -0,0 +1,21 @@
+package se_tpb_speechgen2.tts.adapters;
+
+import se_tpb_speechgen2.tts.TTSException;
+import se_tpb_speechgen2.tts.util.TTSUtils;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class SpeechServerTTS extends AbstractTTSAdapter {
+
+    public SpeechServerTTS(TTSUtils ttsUtils, Map<String, String> params) {
+        super(ttsUtils, params);
+    }
+
+    @Override
+    public void read(String line, File destination) throws IOException, TTSException {
+        throw new NotImplementedException();
+    }
+}
---
+++ transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTSTest.java
@@ -0,0 +1,13 @@
+package se_tpb_speechgen2.tts.adapters;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SpeechServerTTSTest {
+    @Test
+    public void create_speech_server_tts(){
+        assertThat(true, is(true));
+    }
+}
