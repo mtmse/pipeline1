dfb08051ad8f4b0b20b1925288ed5daa8b48fdd8
Thomas Sundberg <tsu@kth.se>
2015-07-01 16:07:33 +0200

    Implement Speech Server TTS

--- transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTS.java
+++ transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTS.java
@@ -1,24 +1,77 @@
 package se_tpb_speechgen2.tts.adapters;
 
+import org.apache.commons.io.FileUtils;
+import se.mtm.speech.synthesis.SpeechClient;
+import se.mtm.speech.synthesis.SynthesizedSound;
 import se_tpb_speechgen2.tts.TTSException;
 import se_tpb_speechgen2.tts.util.TTSUtils;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public class SpeechServerTTS extends AbstractTTSAdapter {
+    private final int maxRetries;
+    private final SpeechClient client;
+
     public SpeechServerTTS(TTSUtils ttsUtils, Map<String, String> params) {
         super(ttsUtils, params);
 
+        String host = params.get("host");
+        if (host == null) {
+            host = "pipeutv1.mtm.se";
+        }
+
+        String stringPort = params.get("port");
+        if (stringPort == null) {
+            stringPort = "9090";
+        }
+        int port = Integer.parseInt(stringPort);
+
+        String stringRetries = params.get("maxRetries");
+        if (stringRetries == null) {
+            stringRetries = "9090";
+        }
+        maxRetries = Integer.parseInt(stringRetries);
+
+        client = new SpeechClient(host, port);
     }
 
     @Override
     public void read(String line, File destination) throws IOException, TTSException {
+        SynthesizedSound sound = client.synthesise(line);
 
+        int retries = 0;
+        while (sound.isNotAccepted() && retries < maxRetries) {
+            pause();
+            sound = client.synthesise(line);
+        }
+        if (sound.isNotAccepted()) {
+            throw new TTSException("line not accepted");
+        }
 
+        retries = 0;
+        while (sound.isTimeout() && retries < maxRetries) {
+            pause();
+            sound = client.synthesise(line);
+        }
+        if (sound.isTimeout()) {
+            throw new TTSException("timeout");
+        }
 
-        throw new NotImplementedException();
+        writeWavFile(sound, destination);
+    }
+
+    private void writeWavFile(SynthesizedSound sound, File soundFile) throws IOException {
+        byte[] content = sound.getSound();
+        FileUtils.writeByteArrayToFile(soundFile, content);
+    }
+
+    private void pause() throws TTSException {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new TTSException(e.getMessage(), e);
+        }
     }
 }
--- transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTSTest.java
+++ transformers/se_tpb_speechgen2/tts/adapters/SpeechServerTTSTest.java
@@ -1,13 +1,35 @@
 package se_tpb_speechgen2.tts.adapters;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import se_tpb_speechgen2.tts.TTSException;
+import se_tpb_speechgen2.tts.util.TTSUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SpeechServerTTSTest {
     @Test
-    public void create_speech_server_tts(){
+    public void create_speech_server_tts() {
         assertThat(true, is(true));
+    }
+
+    @Test
+    @Ignore
+    public void synthesize() throws IOException, TTSException {
+        Map params = new HashMap();
+        TTSUtils ttsUtils = new TTSUtils(params);
+
+        SpeechServerTTS tts = new SpeechServerTTS(ttsUtils, params);
+
+        String sentence = "Den bruna hunden springer omkring och hoppar.";
+        File soundFile = new File("result.wav");
+
+        tts.read(sentence, soundFile);
     }
 }
